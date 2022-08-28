52d96e2996c92ba5bf7f0b6e2b5411205d746c64
Bamtefa Todimu <todimubamtefa11@gmail.com>
2022-08-28 19:30:55 +0100

    user interface of home page

--- twitter-clone/src/components/Main/Home.tsx
+++ twitter-clone/src/components/Main/Home.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+
+import Feed from './Feed'
+import Sidebar from './Sidebar'
+
+import './home.css'
 
 const Home:React.FC= () => {
   return (
-    <div>Welcom to Twitter Clone</div>
+    <div className="home-container">
+      <div className="home-wrapper">
+        <Sidebar/>
+        <Routes>
+          <Route path = "/" element={<Feed/>}/>
+        </Routes>
+      </div>
+    </div>
   )
 }
 
