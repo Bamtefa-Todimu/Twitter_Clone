fb029c098315be97e62d867b0201ca0bf2a544a6
Bamtefa Todimu <todimubamtefa11@gmail.com>
2022-09-07 10:16:54 +0100

    added search bar

--- twitter-clone/src/components/Main/Feed.tsx
+++ twitter-clone/src/components/Main/Feed.tsx
@@ -192,7 +192,16 @@ const Feed:React.FC = () => {
 
           <div className="feed-right-wrapper">
             <div className="feed-right-header">
+                <div className="fr-search">
+                  <svg viewBox="0 0 24 24" aria-hidden="true" width="22px" height="22px" fill='currentColor'><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
+                  <div className="fr-search-input">
+                    <input type="text" name="frsi" id="frsi" placeholder='Search Twitter' />
+                  </div>
 
+                  <div className="clear-search">
+                    <svg viewBox="0 0 15 15" aria-hidden="true" width="10px" height="10px" fill="rgb(113,118,123)"><g><path d="M8.914 7.5l5.793-5.793c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0L7.5 6.086 1.707.293c-.39-.39-1.023-.39-1.414 0s-.39 1.023 0 1.414L6.086 7.5.293 13.293c-.39.39-.39 1.023 0 1.414.195.195.45.293.707.293s.512-.098.707-.293L7.5 8.914l5.793 5.793c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L8.914 7.5z"></path></g></svg>
+                  </div>
+                </div>
             </div>
 
             <div className="feed-right-body">
--- twitter-clone/src/testTweets.ts
+++ twitter-clone/src/testTweets.ts
@@ -48,7 +48,7 @@ const tweets:tweetsType = [
         id:2,
         displayimage:"https://pbs.twimg.com/profile_images/1505899030236172292/3hXjV8X3_400x400.jpg",
         username:"Michigan State Men's Basketball",
-        displayname:"@MSU_Basketball",
+        displayname:"MSU_Basketball",
         content:"Official account of the #SpartanDawgs 🏀",
         video:"https://v16-webapp.tiktok.com/c26966f75605dfd508ff4c0511db44e8/63123851/video/tos/alisg/tos-alisg-pve-0037c001/f17d61d369364c9f9dbd8146b60a1e52/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=3504&bt=1752&cs=0&ds=3&ft=eXd.6HF2Myq8ZFa..he2N1_Aol7Gb&mime_type=video_mp4&qs=0&rc=ZWRmaTU5Njk5M2Y8M2Q0ZkBpajlncjY6ZmxtZDMzODczNEBiMzIzYGJjX18xLWNfMC4zYSNxaGVscjQwcWpgLS1kMS1zcw%3D%3D&l=202209021107170101890780161003E971&btag=80000",
         uploadTime:"1662106130738",
